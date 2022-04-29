554402d8e1d65c618e98749443f557e4643e7542
javierasping <80594036+javierasping@users.noreply.github.com>
2022-04-29 08:47:13 +0200

    Add files via upload

--- js/contacto.js
+++ js/contacto.js
@@ -4,4 +4,6 @@ function cambiarModo() {
     cuerpoweb.classList.toggle("oscuro"); 
 }
 
-
+/*
+let d = new Date();
+document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()</h1>" */
